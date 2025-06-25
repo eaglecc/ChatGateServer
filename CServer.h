0a89f698d4b793adbe601648476d16e08ef70b3e
ChengYuchao <3414906693@qq.com>
2025-06-25 23:35:51 +0800

    初始化GateServer项目

---
+++ CServer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <boost/beast/http.hpp>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
+
+class CServer
+{
+};
+
---
+++ ChatGateServer.cpp
@@ -0,0 +1,14 @@
+﻿#include <json/json.h>
+#include <iostream>
+
+int main() {
+    Json::Value root;
+    root["name"] = "ChatGPT";
+    root["year"] = 2025;
+
+    Json::StreamWriterBuilder writer;
+    std::string output = Json::writeString(writer, root);
+
+    std::cout << output << std::endl;
+    return 0;
+}
