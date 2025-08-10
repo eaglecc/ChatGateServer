2dccf75fc9424443625762208df1112d9f8ac112
ChengYuchao <3414906693@qq.com>
2025-08-10 22:01:46 +0800

    fix：添加注释

--- CServer.h
+++ CServer.h
@@ -4,11 +4,12 @@
 class CServer : public std::enable_shared_from_this<CServer>
 {
 public:
+    // CServer类构造函数接受一个端口号，创建acceptor接受新到来的链接。
     CServer(boost::asio::io_context& ioc, unsigned short& port);
     void Start();
 
 private:
-    tcp::acceptor _acceptor;
+    tcp::acceptor _acceptor; // 用于接受新的TCP连接
     net::io_context& _ioc;
     tcp::socket _socket;
 };
